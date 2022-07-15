1c1b37fd3739499d1ed9d74cd9f347720290e395
FelixMichl <michlfelix@web.de>
2022-07-15 16:24:13 +0200

    enemy1 killable through weapon and exp drops

---
+++ Assets/Scripts/Enemy1Collision.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1Collision : MonoBehaviour
+{
+    public GameObject exp1;
+    public GameObject exp2;
+    public GameObject exp3;
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.name == "Player"){
+            // player should take damage here
+        }
+        else{
+            SpawnExp();
+            Destroy(this.gameObject);
+        }
+        
+    }
+
+    private void SpawnExp(){
+        int n = Random.Range(0,4);
+        switch (n)
+        {
+            case 0:
+            {
+                GameObject e = Instantiate(exp1) as GameObject;
+                e.transform.position = transform.position;
+                break;
+            }
+            case 1:
+            case 2:
+            {
+                GameObject e = Instantiate(exp2) as GameObject;
+                e.transform.position = transform.position;
+                break;
+            }
+            case 3:
+            {   
+                GameObject e = Instantiate(exp3) as GameObject;
+                e.transform.position = transform.position;
+                break;
+            }
+            
+        }
+    }
+}
