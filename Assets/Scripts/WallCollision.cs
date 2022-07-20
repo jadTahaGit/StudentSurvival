c2c2393c97aa2504b91ff3365a67fc69f2db8b4d
FelixMichl <michlfelix@web.de>
2022-07-20 15:32:22 +0200

    adds background

---
+++ Assets/Scripts/WallCollision.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollision : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.name == "Player"){
+            // stop player form going throug wall
+        }
+    }
+
+}
